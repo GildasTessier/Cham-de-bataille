1ca882de8259818bdc96fa61dde9e2fec77723c3
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-08-29 12:03:51 +0200

    add function choice random charters

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -36,26 +36,32 @@ function getcharterAttackerAndDefender (array) {
     while (defender === attacker ) {
         defender = Math.floor(Math.random() * characters.length);
     }
-    return (attacker, defender);
+    return [attacker, defender];
 }
+// console.log(getcharterAttackerAndDefender(characters))
 
 
 // Function to simulate a fight between two characters
-function goBattleBetweenTwoCharacters (attacker, defender) {
+function goBattleBetweenTwoCharacters ([attacker, defender]) {
 
-    const attackerAttack = attacker.xp + Math.floor(Math.random() * (attacker.weaponStrength +1));
-    const defenderDefense = defender.xp + Math.floor(Math.random() * (defender.shieldPower +1));
+    const attackerAttack = characters[attacker].xp + Math.floor(Math.random() * (characters[attacker].weaponStrength +1));
+    const defenderDefense = characters[defender].xp + Math.floor(Math.random() * (characters[defender].shieldPower +1));
 
-    console.log(`!!! Fight between ${attacker.name} and ${defender.name} !!!`);
-    console.log(`${attacker.name} attack with a score of ${attackerAttack} points.`);
-    console.log(`${defender.name} defense with a score of ${defenderDefense} points.`);
+    console.log(`!!! Fight between ${characters[attacker].name} and ${characters[defender].name} !!!`);
+    console.log(`${characters[attacker].name} attack with a score of ${attackerAttack} points.`);
+    console.log(`${characters[defender].name} defense with a score of ${defenderDefense} points.`);
 
     if (attackerAttack > defenderDefense) {
         const damage = attackerAttack - defenderDefense;
-        defender.health -= damage;
-        console.log(`${attacker.name} inflicts ${damage} damage points to ${defender.name}`);
+        characters[defender].health -= damage;
+        console.log(`${characters[attacker].name} inflicts ${damage} damage points to ${characters[defender].name}`);
         if (defender.health <= 0) {
-            console.log(`!!!!!!! ${defender.name} is dead !!!!!!!`);
+            console.log(`!!!!!!! ${characters[defender].name} is dead !!!!!!!`);
         }
     }  
+        else {
+            console.log(`${characters[attacker].name} failed to inflcts damage to ${characters[defender].name}`)
+        }
 }
+console.log(goBattleBetweenTwoCharacters(getcharterAttackerAndDefender(characters)))
+// console.log(goBattleBetweenTwoCharacters(getcharterAttackerAndDefender(characters)))
