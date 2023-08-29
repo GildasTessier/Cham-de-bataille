ffb2bea271a9f586bf28c2fb3bd42ff61347b434
Franckie Moisson <f.moisson.hh14@gmail.com>
2023-08-29 11:20:04 +0200

    battle initilised fight

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -29,3 +29,23 @@ const characters = [
         shieldPower: Math.floor(Math.random() * 10) +1,
     },
 ]
+
+// Function to simulate a fight between two characters
+function goBattleBetweenTwoCharacters (attacker, defender) {
+
+    const attackerAttack = attacker.xp + Math.floor(Math.random() * (attacker.weaponStrength +1));
+    const defenderDefense = defender.xp + Math.floor(Math.random() * (defender.shieldPower +1));
+
+    console.log(`!!! Fight between ${attacker.name} and ${defender.name} !!!`);
+    console.log(`${attacker.name} attack with a score of ${attackerAttack} points.`);
+    console.log(`${defender.name} defense with a score of ${defenderDefense} points.`);
+
+    if (attackerAttack > defenderDefense) {
+        const damage = attackerAttack - defenderDefense;
+        defender.health -= damage;
+        console.log(`${attacker.name} inflicts ${damage} damage points to ${defender.name}`);
+        if (defender.health <= 0) {
+            console.log(`!!!!!!! ${defender.name} is dead !!!!!!!`);
+        }
+    }  
+}
