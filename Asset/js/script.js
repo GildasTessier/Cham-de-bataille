cc256ddbd3489a105b312277d158531427c8ec6e
Franckie Moisson <f.moisson.hh14@gmail.com>
2023-08-29 10:25:06 +0200

    definition des caracteres des joueurs

---
+++ Asset/js/script.js
@@ -0,0 +1,31 @@
+// Definition of characters with their attributes
+const characters = [
+    {
+        name:"Alban",
+        health: 50,
+        xp: Math.floor(Math.random() * 10) +1,
+        weaponStrength: Math.floor(Math.random() * 10) +1,
+        shieldPower: Math.floor(Math.random() * 10) +1,
+    },
+    {
+        name:"Gildas",
+        health: 50,
+        xp: Math.floor(Math.random() * 10) +1,
+        weaponStrength: Math.floor(Math.random() * 10) +1,
+        shieldPower: Math.floor(Math.random() * 10) +1,
+    },
+    {
+        name:"Franckie",
+        health: 50,
+        xp: Math.floor(Math.random() * 10) +1,
+        weaponStrength: Math.floor(Math.random() * 10) +1,
+        shieldPower: Math.floor(Math.random() * 10) +1,
+    },
+    {
+        name:"Oyanna",
+        health: 50,
+        xp: Math.floor(Math.random() * 10) +1,
+        weaponStrength: Math.floor(Math.random() * 10) +1,
+        shieldPower: Math.floor(Math.random() * 10) +1,
+    },
+]
